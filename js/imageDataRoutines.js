f5781d0e6930738305afd7d1ec588931e5cbfce7
cody s <cody@simtable.com>
2019-09-09 14:01:10 -0600

    slight change

--- js/imageDataRoutines.js
+++ js/imageDataRoutines.js
@@ -27,75 +27,85 @@ THE SOFTWARE.
 
 */
 
-  var _Canvas = undefined
-  function createCanvas (w, h) {
-    if (typeof document !== 'undefined') { // nodejs
-      let can = document.createElement('canvas')
-      can.width = w;
-      can.height = h;
-      return can
+var _Canvas = undefined
+function createCanvas(w, h) {
+    if (typeof document !== 'undefined') {
+        // nodejs
+        let can = document.createElement('canvas')
+        can.width = w
+        can.height = h
+        return can
     } else {
-      if (typeof Canvas == 'undefined') {
-        _Canvas = require('canvas')
-      }
-      return new _Canvas.Canvas(w, h)
+        if (typeof Canvas == 'undefined') {
+            _Canvas = require('canvas')
+        }
+        return new _Canvas.Canvas(w, h)
     }
-  }
+}
 
-  export function getIntensities( imgdata){
-    var result = new Float32Array( imgdata.width * imgdata.height)
-    for( var i=0; i < result.length; i++){
-      var indx = i*4
-      // 0.2126 * R + 0.7152 * G + 0.0722 * B
-      result[i] = 0.2126 * imgdata.data[indx] + 0.7152 *imgdata.data[indx+1] + 0.0722 * imgdata.data[indx+2]
+export function getIntensities(imgdata) {
+    var result = new Float32Array(imgdata.width * imgdata.height)
+    for (var i = 0; i < result.length; i++) {
+        var indx = i * 4
+        // 0.2126 * R + 0.7152 * G + 0.0722 * B
+        result[i] =
+            0.2126 * imgdata.data[indx] +
+            0.7152 * imgdata.data[indx + 1] +
+            0.0722 * imgdata.data[indx + 2]
     }
     return result
-  }
+}
 
-  export function getImageCanvas(img){
-      var can = createCanvas(img.width, img.height)
-      var ctx = can.getContext('2d');
-      ctx.drawImage(img,0,0);
-      return ctx
-  }
+export function getImageCanvas(img) {
+    var can = createCanvas(img.width, img.height)
+    var ctx = can.getContext('2d')
+    ctx.drawImage(img, 0, 0)
+    return ctx
+}
 
-  export function convertToImageData( img){
-    if( img.data && img.data instanceof Uint8ClampedArray){//its an image data
-       return img;
-    } else if( img.src ){//its an image
-      return getImageData( img)
-    } else { // assume its a canvas
-      var ctx = img.getContext('2d')
-      var imgData = ctx.getImageData(0,0, img.width, img.height);
-      return imgData
+export function convertToImageData(img) {
+    if (ArrayBuffer.isView(img)) {
+        // it is already some kind of typed array
+        return img
+    } else if (img.data && img.data instanceof Uint8ClampedArray) {
+        // its an image data
+        return img
+    } else if (img.src) {
+        // its an image
+        return getImageData(img)
+    } else {
+        // assume its a canvas
+        var ctx = img.getContext('2d')
+        var imgData = ctx.getImageData(0, 0, img.width, img.height)
+        return imgData
     }
-  }
+}
 
-  export function getImageData(img) {
-      if( img.data && img.data instanceof Uint8ClampedArray){
+export function getImageData(img) {
+    if (img.data && img.data instanceof Uint8ClampedArray) {
         return copyImageData(img)
-      }
-      var ctx = getImageCanvas(img)
-      var imgData = ctx.getImageData(0,0, ctx.canvas.width, ctx.canvas.height);
-      return imgData;
-  }
+    }
+    var ctx = getImageCanvas(img)
+    var imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+    return imgData
+}
 
-  export function imageData2Canvas( imgd){
+export function imageData2Canvas(imgd) {
     var can = createCanvas(imgd.width, imgd.height)
     var ctx = can.getContext('2d')
-    ctx.putImageData(imgd,0,0)
+    ctx.putImageData(imgd, 0, 0)
     return can
-  }
+}
 
-  export function copyImageData(data){
+export function copyImageData(data) {
     var canvas = createCanvas(data.width, data.height)
-    var ctx = canvas.getContext('2d');
-    ctx.putImageData(data,0,0)
-    var imageData = ctx.getImageData(0,0, data.width, data.height)
-    return imageData;
-  }
+    var ctx = canvas.getContext('2d')
+    ctx.putImageData(data, 0, 0)
+    var imageData = ctx.getImageData(0, 0, data.width, data.height)
+    return imageData
+}
 
-  export function createImageData(w,h){
+export function createImageData(w, h) {
     var can = createCanvas(w, h)
-    return convertToImageData( can)
-  }
+    return convertToImageData(can)
+}
